164295a1141f723b7e905813243bb5f5281567c2
vidiyalaswetha29 <143739692+vidiyalaswetha29@users.noreply.github.com>
2024-02-27 17:19:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+// alert("loading");
+function addNewWeField(){
+    // console.log("Adding new field")
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+
+    weOb.insertBefore(newNode, weAddButtonOb);
+}
+
+function addNewAqField(){
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("aqAddButton");
+
+    aqOb.insertBefore(newNode, aqAddButtonOb);
+}
+
+function generateCV(){
+    let nameField = document.getElementById("nameField").value;
+
+    let nameT1 = document.getElementById("nameT1");
+    nameT1.innerHTML = nameField;
+
+    document.getElementById("nameT2").innerHTML = nameField;
+
+    document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+
+    document.getElementById("addressT").innerHTML = document.getElementById("addressField").value;
+
+    document.getElementById("git").innerHTML = document.getElementById("gitHubField").value;
+
+    document.getElementById("linked").innerHTML = document.getElementById("linkedInField").value;
+
+    document.getElementById("objectiveT").innerHTML = document.getElementById("objectiveField").value;
+
+    let wes = document.getElementsByClassName("weField");
+    let str =''
+    for(let e of wes){
+        str = str + `<li> ${e.value} </li>`;
+    }
+    document.getElementById("weT").innerHTML = str;
+    
+    let aqs = document.getElementsByClassName("eqField");
+    let str1 = ''
+    for(let e of aqs){
+        str1 += `<li> ${e.value} </li>`;
+    }
+    document.getElementById("aqT").innerHTML = str1;
+
+    document.getElementById("cv-form").style.display = "none";
+
+    document.getElementById("cv-template").style.display = "block";
+}
+
+function printCV(){
+    window.print();
+}
